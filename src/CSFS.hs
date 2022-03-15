c8f84219f0334ab8f81160242170c0f603bbe5a6
Stephan Schiffels <stephan_schiffels@mac.com>
2022-03-15 17:02:26 +0100

    added table output to CSFS

--- src/CSFS.hs
+++ src/CSFS.hs
@@ -5,7 +5,7 @@ import           Utils                       (PopConfig (..), addGroupDefs,
                                               computeAlleleFreq, readPopConfig)
 
 import           Control.Foldl               (FoldM (..), impurely)
-import           Control.Monad               (unless)
+import           Control.Monad               (unless, forM_)
 import           Control.Monad.IO.Class      (MonadIO, liftIO)
 import qualified Data.ByteString             as B
 import           Data.List                   (intercalate, nub, (\\))
@@ -29,7 +29,9 @@ import           Poseidon.Package            (PackageReadOptions (..),
 import           Poseidon.SecondaryTypes     (IndividualInfo (..))
 import           SequenceFormats.Eigenstrat  (EigenstratSnpEntry (..), GenoLine)
 import           SequenceFormats.Utils       (Chrom (..))
-import           System.IO                   (hPutStrLn, stderr)
+import           System.IO                   (hPutStrLn, stderr, withFile, IOMode(..))
+import           Text.Layout.Table           (asciiRoundS, column, def, expand,
+                                              rowsG, tableString, titlesH)
 
 data CSFSOptions = CSFSOptions
     { _csfsBaseDirs       :: [FilePath]
@@ -82,26 +84,16 @@ runCSFS csfsOpts = do
             let eigenstratProdFiltered = eigenstratProd >-> P.filter (chromFilter (_csfsExcludeChroms csfsOpts))
                     >-> capNrSnps (_csfsMaxSnps csfsOpts)
             impurely P.foldM csfsFold eigenstratProdFiltered
-        print csfsResults
-        -- let tableH = ["Left", "Right", "k", "Cumulative", "Norm", "RAS", "StdErr"]
-        -- let tableB = do
-        --         cumul <- [False, True]
-        --         (i, popLeft) <- zip [0..] popLefts
-        --         (j, popRight) <- zip [0..] popRights
-        --         k <- [0 .. (maxK - 2)]
-        --         let counts = [blockSiteCount bd !! i | bd <- blockData]
-        --             vals = if cumul then
-        --                     [sum [((blockVals bd !! k') !! i) !! j | k' <- [0 .. k]] | bd <- blockData]
-        --                 else
-        --                     [((blockVals bd !! k) !! i) !! j | bd <- blockData]
-        --         let (val, err) = computeJackknife counts vals
-        --         return [show popLeft, show popRight, show (k + 2), show cumul, show (sum counts), show val, show err]
-        -- let colSpecs = replicate 7 (column expand def def def)
-        -- putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableB]
-        -- withFile (_optTableOutFile rasOpts) WriteMode $ \h -> do
-        --     hPutStrLn h $ intercalate "\t" tableH
-        --     forM_ tableB $ \row -> hPutStrLn h (intercalate "\t" row)
-        -- return ()
+        let tableH = ["Left", "Right", "k", "Count"]
+        let tableB = do
+                (k, count) <- zip [0..] (VU.toList csfsResults)
+                return ["None", "All", show k, show count]
+        let colSpecs = replicate 4 (column expand def def def)
+        putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableB]
+        withFile (_csfsTableOutFile csfsOpts) WriteMode $ \h -> do
+            hPutStrLn h $ intercalate "\t" tableH
+            forM_ tableB $ \row -> hPutStrLn h (intercalate "\t" row)
+        return ()
   where
     chromFilter exclusionList (EigenstratSnpEntry chrom _ _ _ _ _, _) = chrom `notElem` exclusionList
     capNrSnps Nothing  = cat
